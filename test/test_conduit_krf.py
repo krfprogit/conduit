1027aa9a805daa7d1a085d02e9428d6f8b1b59be
krfprogit <krfprogit@gmail.com>
2021-08-12 23:19:22 +0200

    Test_5_last page

--- test/test_conduit_krf.py
+++ test/test_conduit_krf.py
@@ -24,6 +24,9 @@ user_login = {"Email": "a@a.hu",
               "Password": "aaaAAA111"
               }
 
+# Test_6_add_new_article
+input_post = ["new", "me", "blabablabal", "key"]
+
 
 ########################################### testing
 
@@ -132,10 +135,13 @@ class TestConduit(object):
 
         print(f"Test_5_scrolling:", end=" ")
         page_lists = self.browser.find_elements_by_class_name("page-link")
+        p_last = []
         for p in page_lists:
             p.click()
             print(p.text, sep=", ", end=" ")
+            p_last.clear()
+            p_last.append(p)
 
-        last_page = xpath(self.browser, f'//*[@class="page-item active" and @data-test="page-link-{p.text}"]')
-        assert (p.text == last_page.text)
+        last_page = xpath(self.browser, f'//*[@class="page-item active" and @data-test="page-link-{p_last.text}"]')
+        assert (p_last.text == last_page.text)
         print(f"last page text: #{last_page.text}")
